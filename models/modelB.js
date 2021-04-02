48ffa9c2984081d43676507e2272fe0054df4248
Megan McBride <megan.mcbride@colorado.edu>
2021-04-02 16:54:19 -0600

    Add object to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developerMeg = {
+  fullName: 'Megan McBride',
+  currentStatus: 'hopeful',
+  heightInInches: 66,
+  yearsCoding: 0,
+  feelingNervous: true,
+  feelingTired: true,
+  favoriteHobbies: ['walking','reading','yoga'],
+  favoriteNumbers: [13,23,77],
+}
