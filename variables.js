132c232c8b656af6e297830c0a9ea2d240b8d180
Megan McBride <megan.mcbride@colorado.edu>
2021-04-02 16:37:17 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,15 @@
+var movie = "Raya and the Last Dragon";
+var isEntertaining = true;
+movie = "Hero";
+
+var num4 = 35;
+var num6 = 27;
+
+var product = num4 * num6;
+
+var favoriteFoods = ['mochi','lettuce wraps','dark chocolate'];
+
+favoriteFoods.shift();
+console.log(favoriteFoods);
+
+console.log(movie.length);
