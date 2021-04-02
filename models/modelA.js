56c44f6ccad83a5d18fc595ac7541fd70714b6a9
Megan McBride <megan.mcbride@colorado.edu>
2021-04-02 16:47:41 -0600

    Add models directory with two files

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name : 'stegasaurus',
+  foodPreference : 'herbivore',
+  avgLengthInFeet : 30,
+  avgAge : 36.2,
+  superCool : true,
+  notSpikey : false,
+  favoriteFoods : ['lettuce','leaves','clover'],
+  bestFriends : ['Ceratosaurus','Brachiosaurus','Alloausrus'],
+}
